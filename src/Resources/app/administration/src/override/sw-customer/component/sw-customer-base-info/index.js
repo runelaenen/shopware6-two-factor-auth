ca5baa228174e92005324dc06f46d9c490d659e6
tinect <s.koenig@tinect.de>
2020-10-13 12:04:01 +0200

    add button in admin to deactivate customers 2FA, closes #13

---
+++ src/Resources/app/administration/src/override/sw-customer/component/sw-customer-base-info/index.js
@@ -0,0 +1,31 @@
+import template from './sw-customer-base-info.twig';
+
+const {Component} = Shopware;
+
+Component.override('sw-customer-base-info', {
+    template,
+
+    computed: {
+        twoFactorAuthenticationActive() {
+            if (!this.customer.customFields) {
+                return false;
+            }
+
+            if (this.customer.customFields.rl_2fa_secret) {
+                return true;
+            }
+
+            return false;
+        }
+    },
+
+    methods: {
+        disable2FA() {
+            if (!this.customer.customFields) {
+                this.$set(this.customer, 'customFields', {});
+            }
+
+            this.customer.customFields.rl_2fa_secret = '';
+        }
+    }
+});
