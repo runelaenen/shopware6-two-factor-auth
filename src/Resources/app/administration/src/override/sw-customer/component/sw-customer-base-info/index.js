98dd7994a69a4bb19131dfbb51f2ea3ac3d4b71f
tinect <s.koenig@tinect.de>
2020-10-22 20:50:32 +0200

    fix SW6.3

--- src/Resources/app/administration/src/override/sw-customer/component/sw-customer-base-info/index.js
+++ src/Resources/app/administration/src/override/sw-customer/component/sw-customer-base-info/index.js
@@ -2,30 +2,32 @@ import template from './sw-customer-base-info.twig';
 
 const { Component } = Shopware;
 
-Component.override('sw-customer-base-info', {
-    template,
+if (Component.getComponentRegistry().has('sw-customer-base-info')) {
+    Component.override('sw-customer-base-info', {
+        template,
 
-    computed: {
-        twoFactorAuthenticationActive() {
-            if (!this.customer.customFields) {
+        computed: {
+            twoFactorAuthenticationActive() {
+                if (!this.customer.customFields) {
+                    return false;
+                }
+
+                if (this.customer.customFields.rl_2fa_secret) {
+                    return true;
+                }
+
                 return false;
             }
+        },
 
-            if (this.customer.customFields.rl_2fa_secret) {
-                return true;
+        methods: {
+            disable2FA() {
+                if (!this.customer.customFields) {
+                    this.$set(this.customer, 'customFields', {});
+                }
+
+                this.customer.customFields.rl_2fa_secret = '';
             }
-
-            return false;
         }
-    },
-
-    methods: {
-        disable2FA() {
-            if (!this.customer.customFields) {
-                this.$set(this.customer, 'customFields', {});
-            }
-
-            this.customer.customFields.rl_2fa_secret = '';
-        }
-    }
-});
+    });
+}
