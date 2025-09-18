855c5f976def41d16023aec1dc260f55a8124151
Bartosz <smietana.bartosz96@gmail.com>
2025-09-18 20:16:39 +0200

    Base main.js fetch on public filesystem operator

--- src/Extension/FileExtension.php
+++ src/Extension/FileExtension.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace RuneLaenen\TwoFactorAuth\Extension;
 
+use League\Flysystem\FilesystemOperator;
 use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Throwable;
@@ -14,28 +15,27 @@ use Twig\TwigFilter;
 class FileExtension extends AbstractExtension
 {
     public function __construct(
-        #[Autowire('%kernel.project_dir%')]
-        private readonly string $projectDir,
+        #[Autowire(service: 'shopware.filesystem.public')]
+        private readonly FilesystemOperator $operator,
     ) {}
 
     public function getFilters(): array
     {
         return [
-            new TwigFilter('main_js', [$this, 'pathToBundleMainJs']),
+            new TwigFilter('main_js', $this->pathToBundleMainJs(...)),
         ];
     }
 
     public function pathToBundleMainJs(string $bundle, string $type = 'administration'): ?string
     {
         try {
-            $path = sprintf(
-                '%s/public/bundles/%s/%s/.vite/manifest.json',
-                $this->projectDir,
+            $content = $this->operator->read(sprintf(
+                '/bundles/%s/%s/.vite/manifest.json',
                 strtolower($bundle),
                 strtolower($type)
-            );
+            ));
 
-            return $type . '/' . json_decode(file_get_contents($path), true)['main.js']['file'] ?? null;
+            return $type . '/' . json_decode($content, true)['main.js']['file'] ?? null;
         } catch (Throwable) {
             return null;
         }
