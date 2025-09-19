f5bdc7fd00cc1594c67e4c4ac34c532f53fe5287
Bartosz <smietana.bartosz96@gmail.com>
2025-09-19 20:33:05 +0200

    Remove prebuilt files

--- src/Resources/public/administration/assets/index-LLQ73JUu.js
+++
@@ -1,2 +0,0 @@
-const e='{% block sw_profile_index_general_password %} <rl-user-otp :user="user" :isLoading="isUserLoading" :onSave="onSave"></rl-user-otp> {% parent %} {% endblock %}',s={template:e,methods:{onSave(){this.$emit("rl-2fa-save")}}};export{s as default};
-//# sourceMappingURL=index-LLQ73JUu.js.map
